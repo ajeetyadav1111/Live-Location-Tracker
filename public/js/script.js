82a6b89e928965b3e7af203b07c8baebe5a40b10
Ajeet Yadav <ajeety4969@gmail.com>
2026-05-13 16:01:27 +0530

    chenge:by

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ if(navigator.geolocation){
 
 const map = L.map("map").setView([0,0], 10)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"WorkCoposer"
+    attribution:"By Ajeet Yadav"
 }).addTo(map)
 
 const markers = {};
